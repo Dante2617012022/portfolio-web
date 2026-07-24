6a7396e1c01ffefe82804f4bfffa5ed3de7ac125
Dante2617012022 <97871421+Dante2617012022@users.noreply.github.com>
2026-07-23 23:37:01 -0300

    revert: quitar controles visuales agregados

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PortfolioDante from './PortfolioDante.jsx'
 import { startPortfolioContentOverrides } from './content-overrides.js'
-import { startVisualEffectsEnhancements } from './visual-effects.js'
 import './index.css'
 
 const redirectPath = sessionStorage.getItem('redirect')
@@ -26,4 +25,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 startPortfolioContentOverrides()
-startVisualEffectsEnhancements()
